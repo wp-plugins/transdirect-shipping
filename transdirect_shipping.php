e1acbf5cf8f6523218d9f5c5714f941df9172641
transdirect <transdirect@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-27 07:17:15 +0000

    Update tags version
    git-svn-id: https://plugins.svn.wordpress.org/transdirect-shipping/tags/1.6@1033629 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- transdirect_shipping.php
+++ transdirect_shipping.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://www.transdirect.com.au/e-commerce/woo-commerce/
  * Description: This plugin allows you to calculate shipping as per your delivery location.
  * FAQ: https://www.transdirect.com.au/e-commerce/woo-commerce/
- * Version: 1.3
+ * Version: 1.6
  * Author: Transdirect
  * Author URI: http://transdirect.com.au/
  * Text Domain: woocommerce_transdirect
